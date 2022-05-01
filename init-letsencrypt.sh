4e460e2d7f02d5d71ecf8897307a53c0d62766ab
maxiang <maxiang@haoshuo.com>
2022-05-01 18:25:10 +0800

    feat: update my email

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -8,7 +8,7 @@ fi
 domains=(api.indeks.cloud)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="theobaldnartha@gmail.com" # Adding a valid address is strongly recommended
+email="1341198325@qq.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
